3c6b7acaf93bf944ebaad8a4ee55466f23f4e98e
Jérôme Fink <jerome.fink@student.unamur.be>
2019-04-05 12:11:22 +0200

    log error

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ check = ModelCheckpoint(
     period=1,
 )
 
+t = train_sequence[0]
+logger.info("Train sequence shape : {}".format(t.shape))
+logger.info("First sequence shape : {}".format(t[0].shape))
+
 model.fit_generator(train_sequence, epochs=100, callbacks=[early_stop, check])
 
 logger.info("Training complete")
